b7031800ae8c79ce96b1db6e1d444ab2cee9ab9f
Ahmed Khaled <163927007+A7MEDK7@users.noreply.github.com>
2024-11-28 17:02:29 +0200

    Update 20_Is_Object_Empty.js

--- 20_Is_Object_Empty.js
+++ 20_Is_Object_Empty.js
@@ -1,6 +1,6 @@
 var isEmpty = function(obj) {
     if(Object.keys(obj).length === 0){
-        return true
+        return true  // Will Return True If Length Of Keys Equal Zero ,So There Is No Key, Then The Object Is Empty
     }
-    return false    
+    return false   // Will Return False If Length Of Keys Equal Al Least One ,So There Is a Key, Then The Object Is Not Empty
 };
